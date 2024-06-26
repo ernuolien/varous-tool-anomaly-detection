89e09fdec72be2c0d6165cc3a8d4a94ce38e7518
Erno <0800dric@gmail.com>
2024-06-26 17:25:23 +0800

    六層新增selfattention但沒什麼用 85.9

--- CAEwithAtt.py
+++ CAEwithAtt.py
@@ -33,16 +33,22 @@ class Encoder(nn.Module):
         self.initcov = nn.Conv1d(130, 16, kernel_size=1)
         
         self.conv1 = nn.Conv1d(16, 32, kernel_size=3, stride=2, padding=3)        
+        self.self_attention1 = SelfAttention(32, 32)
         
         self.conv2 = nn.Conv1d(32, 64, kernel_size=3, stride=2, padding=3)
+        self.self_attention2 = SelfAttention(64, 64)
         
         self.conv3 = nn.Conv1d(64, 128, kernel_size=3, stride=2, padding=3)
+        self.self_attention3 = SelfAttention(128, 128)
         
         self.conv4 = nn.Conv1d(128, 256, kernel_size=3, stride=2, padding=3)
+        self.self_attention4 = SelfAttention(256, 256)
         
         self.conv5 = nn.Conv1d(256, 512, kernel_size=3, stride=2, padding=3)
+        self.self_attention5 = SelfAttention(512, 512)
         
         self.conv6 = nn.Conv1d(512, 1024, kernel_size=3, stride=2, padding=3)
+        self.self_attention6 = SelfAttention(1024, 1024)
 
         
 
@@ -79,17 +85,34 @@ class Encoder(nn.Module):
         x = F.relu(self.initcov(x))
         
         x = F.relu(self.conv1(x))
+        x = x.transpose(1, 2)
+        x = self.self_attention1(x.unsqueeze(1)).squeeze(1)
+        x = x.transpose(1, 2)
         
         x = F.relu(self.conv2(x))
+        x = x.transpose(1, 2)
+        x = self.self_attention2(x.unsqueeze(1)).squeeze(1)
+        x = x.transpose(1, 2)
         
         x = F.relu(self.conv3(x))
+        x = x.transpose(1, 2)
+        x = self.self_attention3(x.unsqueeze(1)).squeeze(1)
+        x = x.transpose(1, 2)
         
         x = F.relu(self.conv4(x))
+        x = x.transpose(1, 2)
+        x = self.self_attention4(x.unsqueeze(1)).squeeze(1)
+        x = x.transpose(1, 2)
         
         x = F.relu(self.conv5(x))
+        x = x.transpose(1, 2)
+        x = self.self_attention5(x.unsqueeze(1)).squeeze(1)
+        x = x.transpose(1, 2)
         
         x = F.relu(self.conv6(x))
-        
+        x = x.transpose(1, 2)
+        x = self.self_attention6(x.unsqueeze(1)).squeeze(1)
+        x = x.transpose(1, 2)
         # # 应用自注意力层
         
         x = torch.flatten(x, start_dim=1)
