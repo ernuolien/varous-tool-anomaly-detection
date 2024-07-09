83c3b93b8e11eb5206f6d6c0452d2e9a46d1f25d
Erno <0800dric@gmail.com>
2024-07-10 01:54:46 +0800

    新增threshold和recall等指標

--- CAEwithAtt_evaulate.py
+++ CAEwithAtt_evaulate.py
@@ -81,6 +81,10 @@ model.eval()
 start_time = time.time()
 with torch.no_grad():
     result_list = []
+    maxloss_list = []
+    minloss_list = []
+    labels_list = []
+    loss_list = []
     for _, (tensors, labels) in enumerate(test_dl):
         
         inputs = tensors.float().to(DEVICE)
@@ -92,14 +96,20 @@ with torch.no_grad():
         # print(loss)
         # total_loss += loss.item()
         loss_per_sample = get_loss_per_sample(inputs, outputs)
+        maxloss_list.append(max(loss_per_sample))
+        minloss_list.append(min(loss_per_sample))
         # print(loss_per_sample)
         for j in range(loss_per_sample.shape[0]):
             result_labels = {k: v[j].item() if isinstance(v, torch.Tensor) else v[j] for k, v in labels.items()}
             result_labels.update(score=loss_per_sample[j].item())
             result_list.append(result_labels)
+            
 
     print(f'Test Loss: {total_loss / len(test_dl):.4f}')
-# print(result_list)
+
+print("avg of the max: ",sum(maxloss_list)/len(maxloss_list))
+print("avg of the min: ",sum(minloss_list)/len(minloss_list))
+# print("avg:",sum(loss_list)/len(loss_list))
 end_time = time.time()
 prediction_time = end_time - start_time
 results = pandas.DataFrame(result_list)
@@ -120,3 +130,31 @@ print("單次預測花費的時間：", prediction_time, "秒")
 
 
 
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import f1_score
+
+
+# 假设实际标签和预测的异常分数如下
+actual_labels = results["anomaly"]
+anomaly_scores = results["score"]
+
+# 设定阈值
+# threshold = 0.74222 f1score = 0.8912 recall = 0.9337 precision = 0.8524
+# threshold = 0.737 f1score = 0.8845 recall = 0.9788 precision = 0.8067
+threshold = 0.737
+
+# 将异常分数转换为预测标签
+predicted_labels = [1 if score > threshold else 0 for score in anomaly_scores]
+
+# 计算准确率
+accuracy = accuracy_score(actual_labels, predicted_labels)
+recall = recall_score(actual_labels, predicted_labels)
+precision = precision_score(actual_labels, predicted_labels)
+f1score = f1_score(actual_labels, predicted_labels)
+
+print("准确率:", accuracy)
+print("recall ", recall)
+print("precision ", precision)
+print("f1score ", f1score)
