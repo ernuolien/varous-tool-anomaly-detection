104cc126b139b02ae7d3572350a299808972766c
Erno <0800dric@gmail.com>
2025-01-02 10:18:51 +0800

    計算recall, f1

--- AE_evaulate.py
+++ AE_evaulate.py
@@ -12,7 +12,7 @@ from sklearn import metrics
 from torch import optim
 
 from configuration import Configuration
-from normalizing_flow import NormalizingFlow, get_loss, get_AXIS_loss_per_sample
+from normalizing_flow import NormalizingFlow, get_loss, get_loss_per_sample
 from voraus_ad import ANOMALY_CATEGORIES, Signals, load_torch_dataloaders
 # from CAE import ConvAutoencoder,Encoder,Decoder
 from AE import AutoEncoder
@@ -76,7 +76,7 @@ scheduler = optim.lr_scheduler.MultiStepLR(
 
 
 
-model.load_state_dict(torch.load("voraus-ad-dataset-main\AE_model32.pth"))
+model.load_state_dict(torch.load("AE_model32.pth"))
 
 total_loss = 0
 model.eval()
@@ -93,7 +93,7 @@ with torch.no_grad():
        
         # print(loss)
         # total_loss += loss.item()
-        loss_per_sample = get_AXIS_loss_per_sample(inputs, outputs)
+        loss_per_sample = get_loss_per_sample(inputs, outputs)
         # print(loss_per_sample)
         for j in range(loss_per_sample.shape[0]):
             result_labels = {k: v[j].item() if isinstance(v, torch.Tensor) else v[j] for k, v in labels.items()}
@@ -127,4 +127,59 @@ print("預測花費的時間：", prediction_time, "秒")
 
 
 
+precision, recall, thresholds = metrics.precision_recall_curve(results["anomaly"], results["score"].values, pos_label=True)
+f1_scores = 2 * (precision * recall) / (precision + recall)
+optimal_idx = numpy.argmax(f1_scores)
+optimal_threshold = thresholds[optimal_idx]
 
+print("Optimal Threshold:", optimal_threshold)
+# 计算 auroc
+# 计算基于最佳阈值的预测标签
+predicted_labels = (numpy.array(results["score"]) >= optimal_threshold).astype(int)
+
+# 计算并打印准确率、召回率、精确率和 F1 分数
+accuracy = numpy.mean(predicted_labels == results["anomaly"])
+recall = recall[optimal_idx]
+precision = precision[optimal_idx]
+f1 = f1_scores[optimal_idx]
+
+print(f'Accuracy: {accuracy}')
+print(f'Recall: {recall}')
+print(f'Precision: {precision}')
+print(f'F1 Score: {f1}')
+# 创建热图
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import f1_score
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+
+normal_anomaly_scores = results[results['anomaly'] == False]['score']
+threshold = numpy.percentile(normal_anomaly_scores, 70)
+# threshold = 10000
+print("Threshold:", threshold)
+actual_labels = results["anomaly"]
+anomaly_scores = results["score"].values    
+predicted_labels = [1 if score > threshold else 0 for score in anomaly_scores]
+
+# 计算准确率
+accuracy = accuracy_score(actual_labels, predicted_labels)
+recall = recall_score(actual_labels, predicted_labels)
+precision = precision_score(actual_labels, predicted_labels)
+f1score = f1_score(actual_labels, predicted_labels)
+print("准确率:", accuracy)
+print("recall ", recall)
+print("precision ", precision)
+print("f1score ", f1score)
+
+conf_matrix = confusion_matrix(actual_labels, predicted_labels)
+
+# 创建热图
+sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=['Pred 0', 'Pred 1'], yticklabels=['True 0', 'True 1'])
+plt.xlabel('Predicted Label')
+plt.ylabel('True Label')
+plt.title('Confusion Matrix')
+plt.show()
+print("t")
